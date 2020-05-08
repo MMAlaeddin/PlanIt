ec323e93bdd86eaca2f726c2a202fc80f4c5ddaa
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-05-08 12:53:14 -0700

    build home controller and start on splash page

--- PlanIt/Controllers/HomeController.cs
+++ PlanIt/Controllers/HomeController.cs
@@ -1,29 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using PlanIt.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace PlanIt.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    [HttpGet("/")]
+    public ActionResult Index()
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      return View();
     }
+  }
 }
