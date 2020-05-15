cb5aa67d68aba676be3115921fcd8de6906522e1
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-05-15 08:37:36 -0700

    build view for editing bus exec summary

--- PlanIt/Models/BusExecSummary.cs
+++ PlanIt/Models/BusExecSummary.cs
@@ -6,6 +6,7 @@ namespace PlanIt.Models
   {
 
       public int SummaryId { get; set; }
+      public string SummaryTitle { get; set; }
       public string Intro { get; set; }
       public string Problem { get; set; }
       public string Financials { get; set; }
