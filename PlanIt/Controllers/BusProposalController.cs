f86813ce77b3e0ae837b1ad2838c2536360a7c6a
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-05-18 09:26:50 -0700

    update one to many to many to many and add Identity to controller

--- PlanIt/Controllers/BusProposalController.cs
+++ PlanIt/Controllers/BusProposalController.cs
@@ -3,67 +3,124 @@ using PlanIt.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Authorization;
-
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace PlanIt.Controllers
 {
+  [Authorize]
   public class BusProposalController : Controller
   {
-    private readonly PlanItContext _db; 
+    private readonly PlanItContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-    public BusProposalController(PlanItContext db)
+    //updated constructor
+    public BusProposalController(UserManager<ApplicationUser> userManager, PlanItContext db)
     {
+      _userManager = userManager;
       _db = db;
     }
-    public ActionResult Index()
+
+    //updated Index method
+    public async Task<ActionResult> Index()
     {
-      List<BusProposal> model = _db.BusProposals.ToList();
-      return View(model);
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      var userBusProposals = _db.BusProposals.Where(entry => entry.User.Id == currentUser.Id);
+      return View(userBusProposals);
     }
+
     public ActionResult Create()
     {
+      ViewBag.ExecSummaryId = new SelectList(_db.ExecSummaries, "ExecSummaryId", "Name");
       return View();
     }
+
+    //updated Create post method
     [HttpPost]
-    public ActionResult Create(BusProposal busProposal)
+    public async Task<ActionResult> Create(BusProposal busProposal, int ExecSummaryId)
     {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      busProposal.User = currentUser;
       _db.BusProposals.Add(busProposal);
+      if (ExecSummaryId != 0)
+      {
+        _db.ExecSummaryBusProposal.Add(new ExecSummaryBusProposal() { ExecSummaryId = ExecSummaryId, BusProposalId = busProposal.BusProposalId });
+      }
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
     public ActionResult Details(int id)
     {
       var thisBusProposal = _db.BusProposals
-        .Include(BusProposal => BusProposal.ExecSummaries)
-        .ThenInclude(join => join.Flavor)
-        .FirstOrDefault(BusProposal => BusProposal.BusProposalId == id);
+          .Include(busProposal => busProposal.ExecSummaries)
+          .ThenInclude(join => join.ExecSummary)
+          .FirstOrDefault(busProposal => busProposal.BusProposalId == id);
       return View(thisBusProposal);
     }
-    // [Authorize]
+
     public ActionResult Edit(int id)
     {
-      BusProposal thisBusProposal = _db.BusProposals.FirstOrDefault(busProposal => busProposal.BusProposalId == id);
+      var thisBusProposal = _db.BusProposals.FirstOrDefault(busProposals => busProposals.BusProposalId == id);
+      ViewBag.ExecSummaryId = new SelectList(_db.ExecSummaries, "ExecSummaryId", "Name");
       return View(thisBusProposal);
     }
+
     [HttpPost]
-    public ActionResult Edit(BusProposal busProposal)
+    public ActionResult Edit(BusProposal busProposal, int ExecSummaryId)
     {
+      if (ExecSummaryId != 0)
+      {
+        _db.ExecSummaryBusProposal.Add(new ExecSummaryBusProposal() { ExecSummaryId = ExecSummaryId, BusProposalId = busProposal.BusProposalId });
+      }
       _db.Entry(busProposal).State = EntityState.Modified;
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
-    // [Authorize]
-    public ActionResult Delete(int id)
+
+    public ActionResult AddCategory(int id)
     {
-      var thisBusProposal = _db.BusProposals.FirstOrDefault(busProposal => busProposal.BusProposalId == id);
+      var thisBusProposal = _db.BusProposals.FirstOrDefault(busProposals => busProposals.BusProposalId == id);
+      ViewBag.ExecSummaryId = new SelectList(_db.ExecSummaries, "ExecSummaryId", "Name");
       return View(thisBusProposal);
     }
+
+    [HttpPost]
+    public ActionResult AddCategory(BusProposal busProposal, int ExecSummaryId)
+    {
+      if (ExecSummaryId != 0)
+      {
+        _db.ExecSummaryBusProposal.Add(new ExecSummaryBusProposal() { ExecSummaryId = ExecSummaryId, BusProposalId = busProposal.BusProposalId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisBusProposal = _db.BusProposals.FirstOrDefault(busProposals => busProposals.BusProposalId == id);
+      return View(thisBusProposal);
+    }
+
     [HttpPost, ActionName("Delete")]
     public ActionResult DeleteConfirmed(int id)
     {
-      var thisBusProposal = _db.BusProposals.FirstOrDefault(busProposal => busProposal.BusProposalId == id);
+      var thisBusProposal = _db.BusProposals.FirstOrDefault(busProposals => busProposals.BusProposalId == id);
       _db.BusProposals.Remove(thisBusProposal);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult DeleteCategory(int joinId)
+    {
+      var joinEntry = _db.ExecSummaryBusProposal.FirstOrDefault(entry => entry.ExecSummaryBusProposalId == joinId);
+      _db.ExecSummaryBusProposal.Remove(joinEntry);
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
